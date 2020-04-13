68deba0667896b5950728976f3472fc5751701c4
unknown <lkshkarki@gmail.com>
2020-04-13 14:07:06 +0530

    promises and observables

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,22 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { DateComponent } from './date.component';
-import { MessageComponent } from './message.component';
-import { Me } from './me.component';
-import { BadgeComponent } from './Components/badge/badge.component';
-import { CardComponent } from './Components/card/card.component';
-import { JumbotronComponent } from './Component/jumbotron/jumbotron.component';
-import {HttpClientModule} from '@angular/common/http';
-import { GridComponent } from './Components/grid/grid.component'
+import { BadgeComponent } from './components/badge/badge.component';
+import { CardComponent } from './components/card/card.component';
 import { SearchPipe } from './pipes/search.pipe';
+import { FirstComponent } from './components/first/first.component';
+import { SecondComponent } from './components/second/second.component';
+
 @NgModule({
   declarations: [
-    AppComponent, DateComponent, BadgeComponent, CardComponent, JumbotronComponent, GridComponent, SearchPipe
+    AppComponent, DateComponent, BadgeComponent, CardComponent, SearchPipe, FirstComponent, SecondComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule, 
+    BrowserModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
