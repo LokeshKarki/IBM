c801c46f40ec5dee392e9320be6004a381c25a80
unknown <lkshkarki@gmail.com>
2020-04-14 09:56:47 +0530

    country - city, parent - child data passing

--- my-first-app/src/app/services/course.service.spec.ts
+++ my-first-app/src/app/services/course.service.spec.ts
@@ -1,16 +1,33 @@
-import { TestBed } from '@angular/core/testing';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-import { CourseService } from './course.service';
+const baseUrl = 'http://localhost:8000/api/employee/'
 
-describe('CourseService', () => {
-  let service: CourseService;
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseService {
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CourseService);
-  });
+  constructor(public http:HttpClient) { }
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+  fetchAllCourses(){
+    
+    // return new Promise((resolve, reject)=> {
+    //   var str = localStorage.getItem('courses')
+    //   if(str){
+    //     var courses = JSON.parse(str)
+    //   }
+    //   else{
+    //     courses = [];
+    //   }
+     
+    //   resolve(courses);
+    // })
+    return this.http.get(baseUrl);
+  }
+
+  deleteCourse(id: number){
+    return this.http.delete(baseUrl + id).toPromise()
+  }
+
+}
